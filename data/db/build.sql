2a793dd9104f88562bc75d3441359495b3fac19b
danmuck <danmuck426@gmail.com>
2021-09-10 19:38:07 -0400

    formatting

--- data/db/build.sql
+++ data/db/build.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS exp (
     UserID integer PRIMARY KEY,
-    UserName text DEFAULT "?",
+    UserName text DEFAULT "n/a",
     XP integer DEFAULT 0,
     Level integer DEFAULT 0,
     XPLock text DEFAULT CURRENT_TIMESTAMP
