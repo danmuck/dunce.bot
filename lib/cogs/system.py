0faf0b8d2095cf7ea0de670e34ccd17470ba875f
danmuck <danmuck426@gmail.com>
2021-09-11 22:12:17 -0400

    tests

--- lib/cogs/system.py
+++ lib/cogs/system.py
@@ -40,7 +40,7 @@ class system(Cog):
         await ctx.send(f"{round(self.client.latency * 1000)}ms")
     
     @command(name = 'ddf87hf73', aliases=['?????????????????????????????'], hidden=True)
-    async def  ddf87hf73(self, ctx):
+    async def ddf87hf73(self, ctx):
         await ctx.send(f'hey {ctx.message.author.display_name} fuck you lol')
 
     # def get_roleT(client, message):
