e223f834d1826f8be9b4cc7893cc60a11d38a7ec
danmuck <danmuck426@gmail.com>
2021-09-13 11:39:07 -0400

    cli testing

--- launcher.py
+++ launcher.py
@@ -1,11 +1,47 @@
 # client import
-from logging import debug
+# from cli.tools import BIC
 from lib.client import client
+
+from logging import debug
+
 # .env import
 import os
 from dotenv import load_dotenv
 load_dotenv()
 VERSION = 'dev'
+
+def BIC(cmd):
+    cmd = input(f'BIC: ')
+
+    # run bot ---
+    if cmd == 'run bot':
+        client.run(VERSION)
+
+    elif cmd == 'test':
+        test = input(f'TESTING [:] ')
+        if test == 'exit':
+            BIC('')
+        if test == 'ftest':
+            input(f'F-TEST [:] ')
+            if 'exit':
+                BIC('')
+        else: BIC('')
+
+    elif cmd == 'db.man ':
+        db_man = input(f'DB.MAN [:] ')
+        if db_man == 'exit':
+            BIC('')
+        else: BIC('')
+
+    elif cmd == 'mess':
+        mess = input(f'MESS [:] ')
+        if mess == 'exit':
+            BIC('')
+
+    else: BIC('')
+
+
+
 
 
 
@@ -18,18 +54,10 @@ LOGIN = os.getenv('LOGIN')
 PASSWORD = os.getenv('PASSWORD')
 if login == LOGIN and password == PASSWORD:
     print(f'\n\n\n\t-[ WELCOME TO DUNCE.BOT ({VERSION}) ]-\n\n')
-
-    # run bot ---
-    BIC = input('BIC: ')
-    if BIC == 'run bot':
-        client.run(VERSION)
-    elif BIC == 'test':
-        print(f'TESTING')
-    elif BIC == 'db man':
-        print(f'testing db man')
-    else: pass
-
+    BIC('')
 
 else:
     print(f'\n\n\n\t-[ ACCESS DENIED ]-\n\n\n\n\n\nexiting program...\n\n\n')
     quit()
+
+
