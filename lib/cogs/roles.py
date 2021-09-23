4914feea36f8622ba6c659fe01f24c5ea79b2a78
danmuck <danmuck426@gmail.com>
2021-09-22 23:21:57 -0400

    roles ID remove

--- lib/cogs/roles.py
+++ lib/cogs/roles.py
@@ -1,9 +1,13 @@
 from discord.ext.commands import Cog, command
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 # notes: set up auto role assign db.record when XP is lvl X assign role X
 from ..db import db
 
+ADMIN_ID = os.getenv('ADMIN_ROLE')
 class roles(Cog):
     def __init__(self, client):
         self.PREFIX = '?'
@@ -21,7 +25,7 @@ class roles(Cog):
     @command(name = "ccc", aliases=["compsci" , "student"], hidden=True)
     # async def ccc(self, ctx):
     async def ccc(self, ctx):
-        await ctx.send(f'``` I am a CCC student  ```<@&881287992382197850>')
+        await ctx.send(f'``` I am a CCC student  ```<@&{ADMIN_ID}>')
 
     @command(name = "joe", aliases=["dr.logic"], hidden=True)
     async def joe(self, ctx):
@@ -29,15 +33,15 @@ class roles(Cog):
 
     @command(name = 'mod', aliases = [ 'moderator' , 'mods' ], hidden=True)
     async def mod(self, ctx):
-        await ctx.send(f'``` **Keep sanity in the public channels primarily** Please communicate with other admins before pushing structural changes. >> mod ```<@&881287992382197850> ')
+        await ctx.send(f'``` **Keep sanity in the public channels primarily** Please communicate with other admins before pushing structural changes. >> mod ```<@&{ADMIN_ID}> ')
 
     @command(name = "admin", aliases=["administrator"], hidden=True)
     async def admin(self, ctx, *, spec='im a god'):
-        await ctx.send(f'``` **This is a contributor role and it is expected that you understand this.** Please communicate with other admins before pushing structural changes. >> admin {spec}```<@&881287992382197850>')
+        await ctx.send(f'``` **This is a contributor role and it is expected that you understand this.** Please communicate with other admins before pushing structural changes. >> admin {spec}```<@&{ADMIN_ID}>')
 
     @command(name = "associate", aliases=["ass" , "as"], hidden=True)
     async def associate(self, ctx, *, friends='friends at ccc?'):
-        await ctx.send(f'``` I would like to hang out with my CCC friends. I know {friends} ```<@&881287992382197850> ')
+        await ctx.send(f'``` I would like to hang out with my CCC friends. I know {friends} ```<@&{ADMIN_ID}> ')
 
     @command(name = 'welcome', aliases=['new', '???????????????'])
     async def  outsider(self, ctx):
